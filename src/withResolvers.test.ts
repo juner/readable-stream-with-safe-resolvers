7a92cd81e7ae6db9d81389034bbaa193a3181bb4
juner <juner@juner.net>
2025-11-15 11:14:42 +0900

    add withResolvers sources

---
+++ src/withResolvers.test.ts
@@ -0,0 +1,100 @@
+import { describe, test } from "vitest";
+import { withResolvers } from "./withResolvers.ts";
+
+describe("simple use", () => {
+  test.concurrent("enqueue and close", async ({ expect }) => {
+    const { enqueue, stream, close } = withResolvers<number>();
+    const executed = (async () => {
+      enqueue(1);
+      await timeout(100);
+      enqueue(2);
+      await timeout(100);
+      close();
+    })();
+    await expect(Array.fromAsync(stream)).resolves.toEqual([
+      1,
+      2
+    ]);
+    await expect(executed).resolves.toBeUndefined();
+  });
+  test.concurrent("error", async ({expect}) => {
+    const { enqueue, stream, error } = withResolvers<number>();
+    const executed = (async () => {
+      enqueue(3);
+      await timeout(100);
+      error(new Error("error"));
+    })();
+    await expect(Array.fromAsync(stream)).rejects.toThrowError("error");
+    await expect(executed).resolves.toBeUndefined();
+  });
+  test.concurrent("for of", async ({expect}) => {
+    const { enqueue, stream } = withResolvers<number>();
+    const executed = (async () => {
+      enqueue(4);
+      await timeout(100);
+      enqueue(5);
+      await timeout(100);
+      enqueue(6);
+    })();
+    for await (const num of stream) {
+      if (num >= 5) break;
+      expect(num).toEqual(4);
+    }
+    await expect(executed).resolves.toBeUndefined();
+  })
+});
+
+describe("safe stop call", () => {
+
+  test.concurrent("enqueue", async ({ expect }) => {
+    const { enqueue, stream, close } = withResolvers<number>();
+    const executed = (async () => {
+      enqueue(1);
+      await timeout(100);
+      close();
+      await timeout(100);
+      enqueue(2);
+    })();
+    await expect(Array.fromAsync(stream)).resolves.toEqual([
+      1
+    ]);
+    await expect(executed).resolves.toBeUndefined();
+  });
+  test.concurrent("close", async ({ expect }) => {
+    const { stream, close } = withResolvers<number>();
+    const executed = (async () => {
+      close();
+      await timeout(100);
+      close();
+    })();
+    await expect(Array.fromAsync(stream)).resolves.toEqual([]);
+    await expect(executed).resolves.toBeUndefined();
+  });
+  test.concurrent("error", async ({ expect }) => {
+    const { enqueue, stream, close, error } = withResolvers<number>();
+    const executed = (async () => {
+      enqueue(4);
+      close();
+      error(new Error("error"));
+    })();
+    await expect(Array.fromAsync(stream)).resolves.toEqual([4]);
+    await expect(executed).resolves.toBeUndefined();
+  });
+});
+
+async function timeout(milliseconds?: number, options?: {
+  signal?: AbortSignal
+}) {
+  const { resolve, promise } = Promise.withResolvers<void>();
+  const clear = setTimeout(resolve, milliseconds);
+  options?.signal?.addEventListener("abort", abort);
+  try {
+    return await promise;
+  } finally {
+    options?.signal?.removeEventListener("abort", abort);
+  }
+  function abort() {
+    clearTimeout(clear);
+    resolve();
+  }
+}
---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, coverageConfigDefaults } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    coverage: {
+      reporter: ["cobertura", "text", "json-summary", "json"],
+      reportsDirectory: "./TestResults/coverage",
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        "src/requiredAsyncDispose.ts",
+        "utils/**/*.ts",
+      ]
+    }
+  }
+});
